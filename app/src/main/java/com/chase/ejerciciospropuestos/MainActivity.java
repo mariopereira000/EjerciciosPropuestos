ca14044f842bc20692057f9d908b2100c4bed385
Alfredo Pereira <mariopereira000@gmail.com>
2020-08-23 22:15:56 -0500

    Sumando y restando

--- app/src/main/java/com/chase/ejerciciospropuestos/MainActivity.java
+++ app/src/main/java/com/chase/ejerciciospropuestos/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
     //cadena para el pintado en la pantalla de operaciones
     private String strCadena;
@@ -28,12 +31,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void procesar(View view) {
         String strTag = view.getTag().toString();
+        String strResultado;
         EditText txtOperaciones = findViewById(R.id.txtOperaciones);
         ///txtOperaciones.setText(strTag + "----" + getString(R.string.Sumar));
         ///Toast.makeText(this,"llamada", Toast.LENGTH_SHORT);
 
         if (strTag.equals(getString(R.string.Ejecutar))) {
-            if(intPosicion > 0) bolPunto = false;
+            if(intPosicion > 0) {
+                // añadimos un salto de linea para mostrar el resultado
+                strResultado = resultado();
+                txtOperaciones.setText(strCadena + " =\n"  + strResultado);
+                //la nueva cadena sera el resultado por si quieren calcular algo despues
+                strCadena = strResultado;
+                bolPunto = false;
+            }
         }
 
         if (strTag.equals(getString(R.string.Borrar))) {
@@ -63,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 bolPunto = true;
                 strTag = ".";
             }else{
-                strTag = "";
+                strTag = "";  // si ya uso el punto ya no lo puede volver a hacer
             }
         }
 
@@ -103,9 +114,52 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        intPosicion++;
-        strCadena = strCadena + strTag;
-        txtOperaciones.setText(strCadena);
+        if (!strTag.equals(getString(R.string.Ejecutar))) {
+            intPosicion++;
+            strCadena = strCadena + strTag;
+            txtOperaciones.setText(strCadena);
+        }
     }
 
+    private String resultado() {
+        String strResultado = new String();
+        String strCadenaAux = new String(strCadena);
+        String strOperacion =new String(),strNumero1 =new String(),strNumero2 =new String();
+        Pattern patPatron = Pattern.compile("(\\d+\\.?\\d*)\\s([\\*]){1}\\s(\\d+\\.?\\d*)");
+        Matcher matMatcher = patPatron.matcher(strCadenaAux);
+        //primero procesamos las multiplicaciones y las divisiones
+        /*while(matMatcher.find()){
+            strNumero1 = matMatcher.group(1).toString();
+            strOperacion = matMatcher.group(2).toString();
+            strNumero2 = matMatcher.group(3).toString();
+
+            if(strOperacion.trim().equals("*")){
+                strResultado = String.valueOf(Float.parseFloat(strNumero1)*Float.parseFloat(strNumero2));
+            }else{
+                strResultado = String.valueOf(Float.parseFloat(strNumero1)/Float.parseFloat(strNumero2));
+            }
+            //reemplazamos el resultado en la cadena original y seguimos operando
+            strCadenaAux = strCadenaAux.replaceFirst(strNumero1 + strOperacion + strNumero2, strResultado);
+        }*/
+
+        //luego procesamos suma y restas
+        patPatron = Pattern.compile("(\\d+\\.?\\d*)\\s([\\+]){1}\\s(\\d+\\.?\\d*)");
+        matMatcher = patPatron.matcher(strCadenaAux);
+        //primero procesamos las multiplicaciones y las divisiones
+        while(matMatcher.find()==true){
+            strNumero1 = matMatcher.group(1).toString();
+            strOperacion = matMatcher.group(2).toString();
+            strNumero2 = matMatcher.group(3).toString();
+
+            if("+".equals(strOperacion.trim())){
+                strResultado = String.valueOf(Float.parseFloat(strNumero1)+Float.parseFloat(strNumero2));
+            }else{
+                strResultado = String.valueOf(Float.parseFloat(strNumero1)-Float.parseFloat(strNumero2));
+            }
+            //reemplazamos el resultado en la cadena original y seguimos operando
+            strCadenaAux = strCadenaAux.replace(strNumero1 + " " + strOperacion + " " + strNumero2, strResultado);
+        }
+
+        return strCadenaAux;
+    }
 }
